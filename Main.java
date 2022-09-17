c683a9feb8087c639c65a3ed33baf74e06200df6
Mustafa Çetinkaya <>
2022-09-17 12:47:05 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+    //DO NOT TOUCH THESE LINE/LINES.  
+    String p1 = "James";
+    String p2 = "Jill";
+    String p3 = "Bob";
+    String p4 = "Betty";
+    String p5 = "Herb";
+    
+    //WRITE YOUR CODE BELOW:
+    System.out.print(p3+", "+p2+", "+p4+", "+p5+", "+p1);
+    
+
+  }
+}
+
+
