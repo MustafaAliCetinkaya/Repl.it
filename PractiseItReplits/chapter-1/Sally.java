a314a6bb19fd2b5b1c9bc2129ef4937a5e870c66
Mustafa Ali Çetinkaya <112425162+MustafaAliCetinkaya@users.noreply.github.com>
2022-11-12 05:27:26 +0300

    Add files via upload
    New replits are uploaded

---
+++ PractiseItReplits/chapter-1/Sally.java
@@ -0,0 +1,10 @@
+/*
+* Write series of println statements that produce the following output
+* "Several slashes are sometimes seen,"
+* said Sally. "I've said so." See?
+* \ / \\ // \\\ ///
+*/
+
+System.out.println("\"Several slashes are sometimes seen,\"");
+System.out.println("said Sally. \"I've said so.\" See?");
+System.out.println("\\ / \\\\ // \\\\\\ ///");
