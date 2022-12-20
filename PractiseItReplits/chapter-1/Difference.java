e5b9f1e9b7cefbaa451863c3b5f4b40f0d1fdd83
Mustafa Ali Çetinkaya <112425162+MustafaAliCetinkaya@users.noreply.github.com>
2022-12-20 23:19:11 +0300

    New replit tasks have been uploaded

--- PractiseItReplits/chapter-1/Difference.java
+++ PractiseItReplits/chapter-1/Difference.java
@@ -1,9 +1,7 @@
 /*
 * Write a complete Java program in a class named Difference that prints the following output:
-*
 * What is the difference between
 * a ' and a "?  Or between a " and a \"?
-*
 * One is what we see when we're typing our program.
 * The other is what appears on the "console."
 */
